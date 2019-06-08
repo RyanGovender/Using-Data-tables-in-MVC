dcb286bab06cc829d4cc1e414199ff2c514a2c54
Ryan <tornaldo07.rg@gmail.com>
2019-06-08 16:20:09 +0200

    Add project files.

---
+++ DataTable/Models/Students.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DataTable.Models
+{
+    public class Students
+    {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        public string StudentNumber { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string Surname { get; set; }
+        [Required]
+        public string ContactNumber { get; set; }
+        [Required]
+        public  double StudentAverage { get; set; }
+    }
+}
